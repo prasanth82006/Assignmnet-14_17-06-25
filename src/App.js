83cb95ed82318cb284e2db486a7fb3ba2748c1c8
P.Prasanth kumar <166972395+prasanth82006@users.noreply.github.com>
2025-06-17 22:47:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,5 @@
-// import './App.css';
-// import Movies from './components/Movies'
 
-// function App() {
-//   return <Movies />;
-// }
-
-// export default App;
-
-// import './App.css';
-import Converter from './components/converter.js'; // Capitalize the import name
-
+import Converter from './components/converter.js'; 
 function App() {
   return <Converter />; // Use the capitalized component name in JSX
 }
